095d64550d093d87848e8bfd9592cfbf97830520
Thomaskutty Reji <thomaskutty13.reji@gmail.com>
2024-02-28 23:27:59 +0530

    updated

---
+++ functions.js
@@ -0,0 +1,22 @@
+
+
+// function declaration 
+function sayMyName(){
+    console.log("thomaskutty")
+}
+sayMyName()
+
+
+// function expression 
+let shoutMyName = function(){
+    console.log("shout thomas")
+}
+
+shoutMyName()
+
+// arrow function 
+let shoutMyNameArr = () =>{
+    console.log("arrow thomas")
+}
+
+shoutMyNameArr()
